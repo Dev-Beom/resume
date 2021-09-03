40b9df04d2ef7db5b000711440e79b9755bf8e29
Dev-Beom <youngbeom0908@gmail.com>
2021-09-04 03:05:12 +0900

    feat : 요약 레이아웃 추가

---
+++ resume/lib/views/layout/summary.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:resume/views/template/dot.dart';
+
+class Summary extends StatefulWidget {
+  const Summary({Key? key}) : super(key: key);
+
+  @override
+  _SummaryState createState() => _SummaryState();
+}
+
+class _SummaryState extends State<Summary> {
+  Size? screenSize;
+
+  @override
+  Widget build(BuildContext context) {
+  }
+}
